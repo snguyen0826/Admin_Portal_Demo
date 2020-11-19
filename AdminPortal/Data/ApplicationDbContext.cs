066e20ffa38856c1193793b89fcc61b8f2f525f5
Sidat Nguyen <sidat.nguyen0826@gmail.com>
2020-11-18 19:01:29 -0600

    First Commit

---
+++ AdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminPortal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Seed Roles
+            builder.Entity<IdentityRole>().HasData
+            (
+                new IdentityRole
+                {
+                    Id = "ADMIN",
+                    Name = "Admin",
+                },
+                new IdentityRole
+                {
+                    Id = "FINANCE_ADMIN",
+                    Name = "Finance Admin",
+                },
+                new IdentityRole
+                {
+                    Id = "SALES_ADMIN",
+                    Name = "Sales Admin",
+                },
+                new IdentityRole
+                {
+                    Id = "HR_ADMIN",
+                    Name = "HR Admin",
+                },
+                new IdentityRole
+                {
+                    Id = "TECH_ADMIN",
+                    Name = "Tech Admin",
+                }
+            );
+        }
+    }
+}
---
+++ AdminPortal/ViewModels/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPortal.ViewModels
+{
+    public class Link
+    {
+        public string LinkUrl { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
